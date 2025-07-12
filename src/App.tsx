e1be22d8b3f129252db1b6446d7eaf8be8180657
goavardanan@iposg.net <goavardanan@iposg.net>
2025-07-12 16:34:49 +0530

    Wellness- App initiated with type script and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <div className="bg-indigo-500 text-white p-4 rounded-xl">
+        Hello Tailwind
+      </div>
+    </div>
+  );
+}
+
+export default App;
